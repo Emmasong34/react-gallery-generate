9c8938a4383e7f02df4b0a2dfed421d424875781
Emma Song Fisher <emmasongf@gmail.com>
2020-10-23 19:14:26 -0500

    galleryItems array updated with image details

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/bread.jpg', description: 'Photo of artisan bread cooling on a rack.', likes: 0 },
+    { id: 2, path: 'images/loom.jpg', description: 'Photo of wooden loom with wool roving and yarn resting on top.', likes: 0 },
+    { id: 3, path: 'images/matt-chips.jpg', description: 'Photo of Matt Harris holding Doctor Chips on an open book.', likes: 0 },
+    { id: 4, path: 'images/modern-dance.jpg', description: 'Photo of Martha Graham pitched forward with wrist on her forehead and leg in a parallel attitude arabesque.', likes: 0 },
+    { id: 5, path: 'images/nature.jpg', description: 'Photo of Wizard island taken from the rim of Crater Lake.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
